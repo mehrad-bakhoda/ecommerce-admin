9c4c363fda0e302e0eb8eed5f9a0acf172bb5640
BlueShdw <no2.nemesis@gmail.com>
2023-11-18 09:41:08 +0330

    final_V1.0

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,4 @@
 "use client"
-import Image from 'next/image'
-import {UserButton} from "@clerk/nextjs";
-import { Modal } from '@/components/ui/modal';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { useEffect } from 'react';
 
